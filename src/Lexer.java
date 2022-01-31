502b366ed6c6d50c75f17aba4656fe4a1b0b2d71
SkylarStewart <stewart.skylard@gmail.com>
2022-01-31 18:13:05 -0500

    minor changes

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,2 +1,28 @@
-public class Lexer {
+import edu.ufl.cise.plc.ILexer;
+import edu.ufl.cise.plc.IToken;
+import edu.ufl.cise.plc.LexicalException;
+
+public class Lexer implements ILexer {
+String input = "124 5677 testtext";
+
+    private enum State {
+        IN_IDENT, //is an identifier
+        RES, // is reserved
+        HAVE_ZERO, //has zero
+        HAVE_DOT, //has dot
+        IN_FLOAT, //has float
+        IN_NUM, //is a num
+        IN_STRING, //parsing as if it were a string
+        IN_COMM, //is a comment
+        IN_WS, //is whitespace
+    }
+    @Override
+    public IToken next() throws LexicalException {
+        return null;
+    }
+
+    @Override
+    public IToken peek() throws LexicalException {
+        return null;
+    }
 }
