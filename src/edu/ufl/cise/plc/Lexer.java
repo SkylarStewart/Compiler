b232c85c1028ecd81f31e0b2bc2eb86a73100188
maddiebrewer <mbrewer1@ufl.edu>
2022-02-01 15:28:09 -0500

    Created map of reserved words

--- src/edu/ufl/cise/plc/Lexer.java
+++ src/edu/ufl/cise/plc/Lexer.java
@@ -2,17 +2,50 @@ package edu.ufl.cise.plc;
 import edu.ufl.cise.plc.ILexer;
 import edu.ufl.cise.plc.IToken;
 import edu.ufl.cise.plc.LexicalException;
+import java.util.HashMap;
 
 public class Lexer implements ILexer {
     String input = "124 5677 testtext";
     int location;
     int length;
     State state;
-
+    static HashMap<String, IToken.Kind> resWords = new HashMap<>();
     public Lexer(String input) {
         this.input = input;
         this.location = 0;
         this.length = input.length();
+        resWords.put("string", IToken.Kind.TYPE);
+        resWords.put("int", IToken.Kind.TYPE);
+        resWords.put("float", IToken.Kind.TYPE);
+        resWords.put("boolean", IToken.Kind.TYPE);
+        resWords.put("color", IToken.Kind.TYPE);
+        resWords.put("image", IToken.Kind.TYPE);
+        resWords.put("void", IToken.Kind.TYPE);
+        resWords.put("getWidth", IToken.Kind.IMAGE_OP);
+        resWords.put("getHeight", IToken.Kind.IMAGE_OP);
+        resWords.put("getRed", IToken.Kind.COLOR_OP);
+        resWords.put("getGreen", IToken.Kind.COLOR_OP);
+        resWords.put("getBlue", IToken.Kind.COLOR_OP);
+        resWords.put("BLACK", IToken.Kind.COLOR_CONST);
+        resWords.put("BLUE", IToken.Kind.COLOR_CONST);
+        resWords.put("CYAN", IToken.Kind.COLOR_CONST);
+        resWords.put("DARK_GRAY", IToken.Kind.COLOR_CONST);
+        resWords.put("GRAY", IToken.Kind.COLOR_CONST);
+        resWords.put("GREEN", IToken.Kind.COLOR_CONST);
+        resWords.put("LIGHT_GRAY", IToken.Kind.COLOR_CONST);
+        resWords.put("MAGENTA", IToken.Kind.COLOR_CONST);
+        resWords.put("ORANGE", IToken.Kind.COLOR_CONST);
+        resWords.put("PINK", IToken.Kind.COLOR_CONST);
+        resWords.put("RED", IToken.Kind.COLOR_CONST);
+        resWords.put("WHITE", IToken.Kind.COLOR_CONST);
+        resWords.put("YELLOW", IToken.Kind.COLOR_CONST);
+        resWords.put("true", IToken.Kind.BOOLEAN_LIT);
+        resWords.put("false", IToken.Kind.BOOLEAN_LIT);
+        resWords.put("if", IToken.Kind.KW_IF);
+        resWords.put("else", IToken.Kind.KW_ELSE);
+        resWords.put("fi", IToken.Kind.KW_FI);
+        resWords.put("write", IToken.Kind.KW_WRITE);
+        resWords.put("console", IToken.Kind.KW_CONSOLE);
     }
 
 
@@ -44,6 +77,7 @@ public class Lexer implements ILexer {
     }
 
     public Token findToken() {
+
         return null;
     }
 }
