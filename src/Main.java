26042768a1fa0ffe9e8da2b115461d1e8e559789
SkylarStewart <stewart.skylard@gmail.com>
2022-02-09 15:06:04 -0500

    added getASCII() to main

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,31 @@ import edu.ufl.cise.plc.Token;
 import edu.ufl.cise.plc.CompilerComponentFactory;
 import edu.ufl.cise.plc.LexicalException;
 import edu.ufl.cise.plc.PLCException;
+
+import java.util.Arrays;
+
+
 public class Main {
+
+    static String getASCII(String s) {
+        int[] ascii = new int[s.length()];
+        for (int i = 0; i != s.length(); i++) {
+            ascii[i] = s.charAt(i);
+        }
+        return Arrays.toString(ascii);
+    }
 
     public static void main(String[] args) throws LexicalException {
         System.out.println("hi!");
         Lexer lexer = new Lexer("""
-                "test
-                amazing test
-                "
-                "test
-                "
-                "test2
-                "
-                "test3" "test4"
-                "test5
-                "
-                + == != #comment
-                11111.33344458 4590059 CYAN void skylar
-                "testtext
                 
-                "
-                     skylar
                 """);
         IToken newToken = lexer.next();
 
         while(newToken.getKind() != IToken.Kind.EOF) {
             System.out.println(newToken.getKind());
-            System.out.println(newToken.getText());
+            System.out.println(newToken.getStringValue());
+            System.out.println(getASCII(newToken.getStringValue()));
            // System.out.println(newToken.getStringValue());
            // System.out.println(newToken.getFloatValue());
             System.out.print(newToken.getSourceLocation().line());
