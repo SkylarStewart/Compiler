52cc3e54fb288809972103792dae3c20b7c11817
maddiebrewer <mbrewer1@ufl.edu>
2022-04-04 16:56:01 -0400

    created shell for CodeGenVisitor

--- src/edu/ufl/cise/plc/CodeGenVisitor.java
+++ src/edu/ufl/cise/plc/CodeGenVisitor.java
@@ -1,4 +1,97 @@
 package edu.ufl.cise.plc;
 
-public class CodeGenVisitor {
+import edu.ufl.cise.plc.ast.*;
+
+public class CodeGenVisitor implements ASTVisitor {
+    CodeGenVisitor(String packageName){
+
+    }
+    public Object visitBooleanLitExpr(BooleanLitExpr booleanLitExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitStringLitExpr(StringLitExpr stringLitExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitIntLitExpr(IntLitExpr intLitExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitFloatLitExpr(FloatLitExpr floatLitExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitColorConstExpr(ColorConstExpr colorConstExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitConsoleExpr(ConsoleExpr consoleExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitColorExpr(ColorExpr colorExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitUnaryExpr(UnaryExpr unaryExpression, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitBinaryExpr(BinaryExpr binaryExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitIdentExpr(IdentExpr identExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitConditionalExpr(ConditionalExpr conditionalExpr, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitDimension(Dimension dimension, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitPixelSelector(PixelSelector pixelSelector, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitAssignmentStatement(AssignmentStatement assignmentStatement, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitWriteStatement(WriteStatement writeStatement, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitReadStatement(ReadStatement readStatement, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitProgram(Program program, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitNameDef(NameDef nameDef, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitNameDefWithDim(NameDefWithDim nameDefWithDim, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitReturnStatement(ReturnStatement returnStatement, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitVarDeclaration(VarDeclaration declaration, Object arg) throws Exception{
+        return null;
+    }
+
+    public Object visitUnaryExprPostfix(UnaryExprPostfix unaryExprPostfix, Object arg) throws Exception{
+        return null;
+    }
+
 }
--- src/edu/ufl/cise/plc/CompilerComponentFactory.java
+++ src/edu/ufl/cise/plc/CompilerComponentFactory.java
@@ -2,7 +2,7 @@ package edu.ufl.cise.plc;
 
 //This class eliminates hard coded dependencies on the actual Lexer class.  You can call your lexer whatever you
 //want as long as it implements the ILexer interface and you have provided an appropriate body for the getLexer method.
-
+import edu.ufl.cise.plc.ast.ASTVisitor;
 
 public class CompilerComponentFactory {
 
@@ -19,6 +19,12 @@ public class CompilerComponentFactory {
 
 	public static TypeCheckVisitor getTypeChecker() {
 		return new TypeCheckVisitor();
+	}
+
+	public static ASTVisitor getCodeGenerator(String packageName) {
+
+		return new CodeGenVisitor(packageName);
+
 	}
 
 }
